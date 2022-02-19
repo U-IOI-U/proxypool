16cc96e7156d9adc2c28cb20e8b90f7dfe797b93
U-IOI-U <74693141+U-IOI-U@users.noreply.github.com>
2022-02-20 05:25:34 +0800

    修改订阅类型的处理逻辑

--- pkg/getter/subscribe.go
+++ pkg/getter/subscribe.go
@@ -5,7 +5,7 @@ import (
 	"io/ioutil"
 	"strings"
 	"sync"
-
+	"gopkg.in/yaml.v3"
 	"github.com/ssrlive/proxypool/pkg/proxy"
 	"github.com/ssrlive/proxypool/pkg/tool"
 )
@@ -32,14 +32,28 @@ func (s *Subscribe) Get() proxy.ProxyList {
 		return nil
 	}
 
-	nodesString, err := tool.Base64DecodeString(string(body))
-	if err != nil {
-		return nil
+	if strings.Contains(string(body), "proxies:") {
+		conf := config{}
+		err = yaml.Unmarshal(body, &conf)
+		if err != nil {
+			return nil
+		}
+	
+		return ClashProxy2ProxyArray(conf.Proxy)
+	} else {
+		if strings.Contains(string(body), "ss://") || strings.Contains(string(body), "ssr://") || strings.Contains(string(body), "vmess://") || strings.Contains(string(body), "trojan://") {
+			return FuzzParseProxyFromString(string(body))
+		} else {
+			nodesString, err := tool.Base64DecodeString(string(body))
+			if err != nil {
+				return nil
+			}
+			nodesString = strings.ReplaceAll(nodesString, "\t", "")
+		
+			nodes := strings.Split(nodesString, "\n")
+			return StringArray2ProxyArray(nodes)
+		}
 	}
-	nodesString = strings.ReplaceAll(nodesString, "\t", "")
-
-	nodes := strings.Split(nodesString, "\n")
-	return StringArray2ProxyArray(nodes)
 }
 
 // Get2Chan() of Subscribe is to implement Getter interface. It gets proxies and send proxy to channel one by one
