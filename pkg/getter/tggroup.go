a6571c0dfc68936c49e8fcc693785d50bce5db9f
U-IOI-U <74693141+U-IOI-U@users.noreply.github.com>
2022-02-22 09:59:02 +0800

    TG组类型添加分页获取

--- pkg/getter/tggroup.go
+++ pkg/getter/tggroup.go
@@ -47,30 +47,41 @@ func NewTGGroupGetter(options tool.Options) (getter Getter, err error) {
 		return &TGGroupGetter{
 			c:         tool.GetColly(),
 			NumNeeded: t,
-			Url:       C.Config.TGFileApi + url + "?limit=" + strconv.Itoa(t),
+			Url:       C.Config.TGFileApi + url,
 		}, nil
 	}
 	return nil, ErrorUrlNotFound
 }
 
 func (g *TGGroupGetter) Get() proxy.ProxyList {
-	// 获取文件(api需要维护)
-	resp, err := tool.GetHttpClient().Get(g.Url)
-	if err != nil {
-		return nil
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil
-	}
-
+	onePageMsg := 100
+	subHtmls := make([]string, 0)
 	result := make(proxy.ProxyList, 0)
+	
+	for pageNum, j := 1, g.NumNeeded; j > 0; pageNum, j = pageNum + 1, j - onePageMsg {
+		// 获取文件(api需要维护)
+		resp, err := tool.GetHttpClient().Get(g.Url + "/" + strconv.Itoa(pageNum) + "?limit=" + strconv.Itoa(onePageMsg))
+		if err != nil {
+			break
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			break
+		}
+		subHtmls = append(subHtmls, string(body))
+	}
+	// 没有获取到网页
+	if len(subHtmls) == 0 {
+		return nil
+	}
+	
+	allHtmls := strings.Join(subHtmls, " ")
 	// 获取内容中的 vmess://, ss://, ssr://, trojan:// 等格式的节点信息
-	result = append(result, StringArray2ProxyArray(GrepLinksFromString(string(body)))...)
+	result = append(result, StringArray2ProxyArray(GrepLinksFromString(allHtmls))...)
 
 	// 抓取到http链接，有可能是订阅链接或其他链接，无论如何试一下
-	subUrls := FindAllSubscribeUrl(html.UnescapeString(string(body)), -1)
+	subUrls := FindAllSubscribeUrl(html.UnescapeString(allHtmls), -1)
 	for _, url := range subUrls {
 		// 屏蔽掉无效的链接
 		if strings.HasPrefix(url, "https://t.me") {
